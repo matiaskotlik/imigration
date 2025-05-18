054eeb497fb861f87f923de86f8019eaf0a390e7
Matias Kotlik <matiaskotlik@users.noreply.github.com>
2025-05-18 17:25:35 -0400

    don't redirect to survey

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import {Redirect} from 'expo-router';
 
 export default function Home() {
   if (process.env.NODE_ENV === 'development') {
-    return <Redirect href='/survey'/>;
+    // return <Redirect href='/survey'/>;
   }
 
   return <Redirect href='/onboarding'/>;
