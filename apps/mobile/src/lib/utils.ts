76cd353d2c1ff85983d58fc20a8f2e1c37fea4ac
Matias Kotlik <matiaskotlik@users.noreply.github.com>
2025-06-14 07:42:07 -0400

    fix: build

--- apps/mobile/src/lib/utils.ts
+++ apps/mobile/src/lib/utils.ts
@@ -8,5 +8,5 @@ export async function raiseStatus(res: Response) {
 }
 
 export async function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise<void>((resolve) => setTimeout(resolve, ms));
 }
