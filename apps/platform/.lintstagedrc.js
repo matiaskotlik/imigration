9b39ae05047e6ea95f76da82d958659c4fb31fdc
Matias Kotlik <matiaskotlik@users.noreply.github.com>
2025-06-15 18:47:52 -0400

    fix: lintstaged

--- apps/platform/.lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-const nextLintCommand = (filenames) =>
-  `pnpm lint ${filenames.map((f) => `--file ${path.relative(process.cwd(), f)}`)}`
-
-module.exports = {
-  "**": ["pnpm format", nextLintCommand]
-}
-
