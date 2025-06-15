b67107396e6f6bcc3bd4589aca60738eb4f07273
Matias Kotlik <matiaskotlik@users.noreply.github.com>
2025-06-15 04:43:01 -0400

    fix: vercel config

--- apps/platform/next.config.js
+++ apps/platform/next.config.js
@@ -5,7 +5,7 @@ const jiti = createJiti(import.meta.url);
 // Import env files to validate at build time. Use jiti so we can load .ts files in here.
 await jiti.import('./src/env');
 
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   /** Enable hot reloading for local packages without a build step */
   transpilePackages: ['@repo/api', '@repo/pdfme-plugins', '@repo/supabase'],
